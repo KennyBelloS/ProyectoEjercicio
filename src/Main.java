15cced3809d7d9534e1c76e1750edd07d62d3390
Kenny Bello <kenn.ybello555@gmail.com>
2024-09-18 14:27:25 -0500

    prueba

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("hola");
+        System.out.println();
+        System.out.println();
+    }
+
+
+}
